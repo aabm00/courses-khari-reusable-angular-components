cc96432f2e53bde47284e08e086646da1c1f6083
Toni <aabm00@gmail.com>
2026-07-21 19:08:19 +0200

    CH08-01: Video 79

--- toni/CH8/01-microsyntax-playground/src/app/directives/my-repeat.directive.ts
+++ toni/CH8/01-microsyntax-playground/src/app/directives/my-repeat.directive.ts
@@ -1,8 +1,18 @@
-import { Directive } from "@angular/core";
+import { Directive, input } from "@angular/core";
+
+/** ANGULAR MICROSYNTAX INPUT NAMING RULE:
+ * Secondary inputs MUST be named using the directive's selector as a prefix
+ * followed by the parameter name in CamelCase (e.g., 'myRepeat' + 'Start' = 'myRepeatStart').
+ * If you name the input simply 'start', Angular's micro-syntax parser will fail to map
+ * the 'start: 6' expression from the HTML to this directive.
+ */
+
 
 @Directive({
     selector: '[myRepeat]'
 })
 export class MyRepeat {
-
+  readonly myRepeat = input.required<number>()
+  readonly myRepeatStart = input(0)
+  readonly myRepeatSkip = input(1)
 }
