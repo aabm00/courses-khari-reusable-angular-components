0a725e638aac995fb3472b2bec9a2b554157c3a1
Toni <aabm00@gmail.com>
2026-07-22 15:11:05 +0200

    CH8-01: Video 86

--- toni/CH8/01-microsyntax-playground/src/app/directives/my-for.directive.ts
+++ toni/CH8/01-microsyntax-playground/src/app/directives/my-for.directive.ts
@@ -1,8 +1,38 @@
-import { Directive } from "@angular/core";
+import { Directive, input } from "@angular/core";
+
+export interface MyForContext {
+  readonly $implicit: any
+  readonly index: number
+  readonly first: boolean
+  readonly last: boolean
+  readonly odd: boolean
+  readonly even: boolean
+}
+
+export type MyForTrackBy = (item: any, index: number) => any
+
 
 @Directive({
     selector: '[myFor]'
 })
 export class MyFor {
+
+  readonly myForOf = input.required<any[]>() // of isn't a reserved keyword in MicroSystax only let and as are.
+  readonly myForTrackBy = input<MyForTrackBy>( (item, index) => item )
+
+
+  /**
+   * TEMPLATE CONTEXT GUARD (Strict HTML Type Checking):
+   *
+   * - Purpose: Binds the 'MyRepeatContext' interface directly to the HTML template.
+   *   Without this, variables like 'let value' or 'let i = index' would be typed
+   *   as 'any', losing autocomplete and risking silent runtime typos.
+   *
+   * - When to use: ALWAYS include this method when building custom structural
+   *   directives that export data back to the template using context variables.
+   */
+  static ngTemplateContextGuard(_: MyFor, ctx: unknown): ctx is MyForContext {
+    return true;
+  }
 
 }
--- toni/CH8/01-microsyntax-playground/src/app/directives/my-if.directive.ts
+++ toni/CH8/01-microsyntax-playground/src/app/directives/my-if.directive.ts
@@ -1,8 +1,33 @@
-import { Directive } from "@angular/core";
+import { Directive, input } from "@angular/core";
+
+export interface MyIfContext {
+  // Inputs passed as context
+  readonly myIf: any
+}
+
 
 @Directive({
     selector: '[myIf]'
 })
 export class MyIf {
+
+  readonly myIf = input.required<any>() // Because it must accept truty values not only booleans
+
+
+
+
+  /**
+   * TEMPLATE CONTEXT GUARD (Strict HTML Type Checking):
+   *
+   * - Purpose: Binds the 'MyRepeatContext' interface directly to the HTML template.
+   *   Without this, variables like 'let value' or 'let i = index' would be typed
+   *   as 'any', losing autocomplete and risking silent runtime typos.
+   *
+   * - When to use: ALWAYS include this method when building custom structural
+   *   directives that export data back to the template using context variables.
+   */
+  static ngTemplateContextGuard(_: MyIf, ctx: unknown): ctx is MyIfContext {
+    return true;
+  }
 
 }
