ec4858951a5294e6970411c573d47974dea89154
Toni <aabm00@gmail.com>
2026-07-22 17:27:27 +0200

    CH9-01: Videos 93 - 94

--- toni/CH9/01-microsyntax-playground-myif/src/app/directives/my-if.directive.ts
+++ toni/CH9/01-microsyntax-playground-myif/src/app/directives/my-if.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, input } from "@angular/core";
+import { computed, Directive, effect, inject, input, TemplateRef, ViewContainerRef } from '@angular/core';
 
 export interface MyIfContext<T> {
     readonly myIf: T;
@@ -8,10 +8,39 @@ export interface MyIfContext<T> {
     selector: '[myIf]'
 })
 export class MyIf<T> {
-    readonly myIf = input.required<T>();
 
-    static ngTemplateContextGuard<T>(_: MyIf<T>, ctx: unknown): ctx is MyIfContext<T> {
-        return true;
+  readonly template = inject<TemplateRef<MyIfContext<T>>>(TemplateRef)
+  readonly vcr = inject(ViewContainerRef)
+
+  readonly myIf = input.required<T>();
+
+  readonly condition = computed(() => !!this.myIf())
+
+  private invalidate() {
+    const cond = this.condition()
+    if(cond) {
+      this.vcr.clear()
+      const ctx: MyIfContext<T> = {
+        myIf: this.myIf()
+      }
+      this.vcr.createEmbeddedView(this.template, ctx)
     }
+    else {
+      if(this.vcr.length > 0) {
+        this.vcr.clear()
+      }
+    }
+  }
+
+  constructor() {
+    effect(() => {
+      this.invalidate()
+    })
+  }
+
+  static ngTemplateContextGuard<T>(_: MyIf<T>, ctx: unknown): ctx is MyIfContext<T> {
+      return true;
+  }
+
 
 }
